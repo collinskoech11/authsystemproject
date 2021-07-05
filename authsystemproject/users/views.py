88088793dd8dbea77be2bf2081166bc99dd54a05
collinskoech11 <collinskoechck34@gmail.com>
2021-07-05 13:07:19 +0300

    email field registration form

--- authsystemproject/users/views.py
+++ authsystemproject/users/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
+from . forms import UserRegisterForm
 from django.contrib import messages
 # Create your views here.
 def home(request):
@@ -7,13 +8,13 @@ def home(request):
 
 def register(request):
     if request.method == "POST":
-        form = UserCreationForm(request.POST)
+        form = UserRegisterForm(request.POST)
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
             messages.success(request, f'Hi {username}, Your account was created successfully ')
             return redirect('home')
     else:
-        form = UserCreationForm()
+        form = UserRegisterForm()
 
     return render(request, 'users/register.html', {'form':form})
