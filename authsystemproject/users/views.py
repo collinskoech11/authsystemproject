a499c97e8361dadeecb5d6698aa32269418efc19
collinskoech11 <collinskoechck34@gmail.com>
2021-07-05 14:48:12 +0300

    user auth

--- authsystemproject/users/views.py
+++ authsystemproject/users/views.py
@@ -18,3 +18,8 @@ def register(request):
         form = UserRegisterForm()
 
     return render(request, 'users/register.html', {'form':form})
+
+
+
+def profile(request):
+    return render(request, 'users/profile.html')
